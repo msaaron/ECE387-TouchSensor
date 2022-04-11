89cd98ff75aa5e39fb2a5af176dfe2c7df907618
Matt Aaron <13080357+msaaron@users.noreply.github.com>
2022-04-10 22:37:48 -0400

    Create compile_script.py

---
+++ src/compile_script.py
@@ -0,0 +1,54 @@
+-import sys
+import os
+import subprocess
+import random
+import operator
+import csv
+import logging
+import tempfile
+
+def run_command(command):
+    print('----------------------------------------')
+    print('Executing: '+command)
+    pipe = subprocess.Popen(command, shell=True, bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+    #pipe.stdout.close()
+    out, err = pipe.communicate()
+    print (out)
+    
+
+# 0_script 1_list_of_student_usernames 2_directory_to_store_files
+# SAMPLE: compile_script.py level_of_compilation
+print ("Number of arguments: %d" %  len(sys.argv))
+print ("Argument List: %s" % str(sys.argv))
+
+files_to_compile = ['main']
+directories = []
+file_to_compile = 'main'
+if len(sys.argv) == 2:
+    com_port = 'com6'
+else:
+	com_port = sys.argv[2]
+
+level_of_compilation = int(sys.argv[1])
+
+path_win_avr = 'c:/avr-gcc-11.1.0/bin/'
+
+if level_of_compilation >= 1: 
+    for fs in files_to_compile:
+        cmd = path_win_avr + 'avr-gcc.exe -Wall -Os -DF_CPU=16-000UL -mmcu=atmega328p -c -o'+fs+'.o '+fs+'.c'
+        run_command(cmd)
+    
+if level_of_compilation >= 2:
+    cmd = path_win_avr + 'avr-gcc -mmcu=atmega328p '
+    for fs in files_to_compile:
+        cmd = cmd+fs+'.o ' 
+    cmd = cmd +'-o '+file_to_compile
+    run_command(cmd)
+
+if level_of_compilation >= 3:
+    cmd = path_win_avr + 'avr-objcopy -O ihex -R .eeprom '+file_to_compile+' '+file_to_compile+'.hex'
+    run_command(cmd)
+
+if level_of_compilation >= 4:
+    cmd = path_win_avr + 'avrdude.exe -patmega328p -P'+com_port+' -carduino -D -U flash:w:'+file_to_compile+'.hex:i'
+    run_command(cmd)
